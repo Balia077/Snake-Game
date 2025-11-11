f53a77cbfacba12f8115ca3fd78e3e04b7df0c5e
Balia077 <balaramd894@gmail.com>
2025-11-11 20:45:05 +0530

    Implement food generation and game over alert in snake game logic; add food styling in CSS

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const blockWidth = 30
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let intervalId = null;
+let food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)}
 
 const blocks = []
 const snake = [
@@ -23,14 +25,9 @@ for(let row = 0; row < rows; row++){
 }
 
 function render(){
-    snake.forEach(segment => {
-        blocks[`${segment.x}-${segment.y}`].classList.add('fill');
-        
-    })
-}
+     let head = null
 
-setInterval(() => {
-    let head = null
+     blocks[`${food.x}-${food.y}`].classList.add('food');
 
     if(direction === 'left'){
         head = {x: snake[0].x, y: snake[0].y - 1}
@@ -45,15 +42,35 @@ setInterval(() => {
         head = {x: snake[0].x - 1, y: snake[0].y}
     }
 
+    if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
+        alert("Game Over");
+        clearInterval(intervalId);
+    }
+
+    if(head.x === food.x && head.y === food.y){
+        blocks[`${food.x}-${food.y}`].classList.remove('food');
+        food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)};
+        blocks[`${food.x}-${food.y}`].classList.add('food');
+
+        snake.unshift(head)
+    }
+
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
     })
 
-    /* snake.unshift(head)
-    snake.pop() */
+    snake.unshift(head)
+    snake.pop()
 
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add('fill');
+        
+    })
+}
+
+intervalId = setInterval(() => {
     render()
-}, 400);
+}, 200);
 
 addEventListener("keydown", (event) => {
     if(event.key === "ArrowUp"){
